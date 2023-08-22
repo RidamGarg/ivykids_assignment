a55cbc7b4d9503bfa0fbcee520f83ce2ca24cdf6
RidamGarg <ridamgarg8979@gmail>
2023-08-22 14:37:14 +0530

    first commit

--- server/src/app.js
+++ server/src/app.js
@@ -23,9 +23,9 @@ app.use("/auth", auth, require("./routers/profile"));
 app.use("/secure", require("./routers/secure"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve("../../client", "build")));
+  app.use(express.static(path.resolve("../client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("../../client", "build", "index.html"));
+    res.sendFile(path.resolve("../client", "build", "index.html"));
   });
 }
 
